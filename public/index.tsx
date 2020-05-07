baedf6d589844a166fc5555f3ddea8932a3bf274
maximdegeyter <maximdegeyter@outlook.com>
2020-05-07 14:17:44 +0200

    feat: move to tenantlevel

--- public/index.tsx
+++ public/index.tsx
@@ -5,25 +5,13 @@ import { Redirect } from 'react-router-dom';
 import { registerRoutes } from './lib/connectors/sites';
 import { TenantContext } from './lib/context';
 import { MODULE_PATHS } from './lib/roles.const';
-import { RolesRouteProps } from './lib/roles.types';
-import { RolesOverview, UsersOverview } from './lib/views';
+import { RolesModuleProps } from './lib/roles.types';
+import { UsersOverview } from './lib/views';
 
-const RolesComponent: FC<RolesRouteProps> = ({ route, location, match, tenantId }) => {
-	// if path is /rights, redirect to /rights/overzicht
-	if (
-		/\/\b[0-9a-f]{8}\b-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-\b[0-9a-f]{12}\b\/roles$/.test(
-			location.pathname
-		)
-	) {
-		return <Redirect to={`${location.pathname}/overzicht`} />;
-	}
+const RolesComponent: FC<RolesModuleProps> = ({ route, location, match, tenantId }) => {
 	// if path is /users, redirect to /users/overzicht
-	if (
-		/\/\b[0-9a-f]{8}\b-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-\b[0-9a-f]{12}\b\/users$/.test(
-			location.pathname
-		)
-	) {
-		return <Redirect to={`${location.pathname}/overzicht`} />;
+	if (/\/users$/.test(location.pathname)) {
+		return <Redirect to={`/${tenantId}/users/overzicht`} />;
 	}
 
 	return (
@@ -37,47 +25,20 @@ const RolesComponent: FC<RolesRouteProps> = ({ route, location, match, tenantId 
 	);
 };
 
-registerRoutes({
-	path: MODULE_PATHS.root,
+Core.routes.register({
+	path: '/users',
 	component: RolesComponent,
 	navigation: {
-		renderContext: 'site',
-		context: 'site',
 		label: 'Gebruikers',
 	},
-	exact: true,
 	routes: [
 		{
-			path: MODULE_PATHS.roles.root,
-			component: RolesOverview,
-			navigation: {
-				renderContext: 'site',
-				context: 'site',
-				label: 'Rollen en rechten',
-				parentPath: MODULE_PATHS.root,
-			},
-			routes: [
-				{
-					path: MODULE_PATHS.roles.overview,
-					component: RolesOverview,
-				},
-			],
-		},
-		{
-			path: MODULE_PATHS.users.root,
+			path: '/users/overzicht',
 			component: UsersOverview,
 			navigation: {
-				renderContext: 'site',
-				context: 'site',
 				label: 'Gebruikers',
-				parentPath: MODULE_PATHS.root,
+				parentPath: '/users',
 			},
-			routes: [
-				{
-					path: MODULE_PATHS.users.overview,
-					component: UsersOverview,
-				},
-			],
 		},
 	],
 });
