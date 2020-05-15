0201d37d5f98444fe395343527553d7eff6ab997
maximdegeyter <maximdegeyter@outlook.com>
2020-05-15 16:13:13 +0200

    fix: fix sites connector

--- public/lib/connectors/sites.ts
+++ public/lib/connectors/sites.ts
@@ -2,9 +2,9 @@ import Core, { ModuleRouteConfig } from '@redactie/redactie-core';
 import { Routes } from '@redactie/redactie-core/dist/routes';
 
 // TODO export sites api typings
-const RolesAPI: { routes: Routes } = Core.modules.getModuleAPI('roles-rights-module') as {
+const SitesAPI: { routes: Routes } = Core.modules.getModuleAPI('sites-module') as {
 	routes: Routes;
 };
 
 export const registerRoutes = (routes: ModuleRouteConfig): any | false =>
-	RolesAPI ? RolesAPI.routes.register(routes) : false;
+	SitesAPI ? SitesAPI.routes.register(routes) : false;
