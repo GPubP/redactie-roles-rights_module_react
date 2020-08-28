425b3f571f05a42200ef87ac4f1140200bc08854
De Weerdt Maarten <maarten.deweerdt@cronos.be>
2020-08-28 14:39:51 +0200

    fix(red-307): roledetailform submit delta

--- public/lib/components/forms/RoleDetailForm/RoleDetailForm.tsx
+++ public/lib/components/forms/RoleDetailForm/RoleDetailForm.tsx
@@ -8,10 +8,12 @@ import {
 } from '@acpaas-ui/react-components';
 import { ActionBar, ActionBarContentSection } from '@acpaas-ui/react-editorial-components';
 import { CORE_TRANSLATIONS } from '@redactie/translations-module/public/lib/i18next/translations.const';
+import { useDetectValueChanges } from '@redactie/utils';
 import { Field, Formik } from 'formik';
-import React, { FC, ReactElement } from 'react';
+import React, { FC, ReactElement, useState } from 'react';
 
 import { useCoreTranslation } from '../../../connectors/translations';
+import { RoleDetailFormState } from '../../../roles.types';
 
 import { ROLE_DETAIL_VALIDATION_SCHEMA } from './RoleDetailForm.const';
 import { RoleDetailFormProps } from './RoleDetailForm.types';
@@ -25,6 +27,8 @@ const RoleDetailForm: FC<RoleDetailFormProps> = ({
 	onDelete,
 }) => {
 	const [t] = useCoreTranslation();
+	const [formValue, setFormValue] = useState<RoleDetailFormState | null>(null);
+	const [isChanged] = useDetectValueChanges(!isLoading, formValue);
 
 	const renderDelete = (): ReactElement => {
 		return (
@@ -51,50 +55,54 @@ const RoleDetailForm: FC<RoleDetailFormProps> = ({
 			onSubmit={onSubmit}
 			validationSchema={ROLE_DETAIL_VALIDATION_SCHEMA}
 		>
-			{({ submitForm }) => (
-				<>
-					<div className="row">
-						<div className="col-xs-12 col-md-6">
-							<Field as={TextField} id="name" label="Naam" name="name" />
-							<div className="u-text-light u-margin-top-xs">
-								Geef de rol een korte en duidelijke naam.
+			{({ submitForm, values }) => {
+				setFormValue(values);
+
+				return (
+					<>
+						<div className="row">
+							<div className="col-xs-12 col-md-6">
+								<Field as={TextField} id="name" label="Naam" name="name" />
+								<div className="u-text-light u-margin-top-xs">
+									Geef de rol een korte en duidelijke naam.
+								</div>
 							</div>
 						</div>
-					</div>
-					<div className="row u-margin-top">
-						<div className="col-xs-12">
-							<Field
-								as={Textarea}
-								id="description"
-								label="Beschrijving"
-								name="description"
-							/>
-							<div className="u-text-light u-margin-top-xs">
-								Geef de rol een duidelijke beschrijving voor in het overzicht
+						<div className="row u-margin-top">
+							<div className="col-xs-12">
+								<Field
+									as={Textarea}
+									id="description"
+									label="Beschrijving"
+									name="description"
+								/>
+								<div className="u-text-light u-margin-top-xs">
+									Geef de rol een duidelijke beschrijving voor in het overzicht
+								</div>
 							</div>
 						</div>
-					</div>
-					{onDelete ? renderDelete() : null}
-					<ActionBar className="o-action-bar--fixed" isOpen>
-						<ActionBarContentSection>
-							<div className="u-wrapper row end-xs">
-								<Button onClick={onCancel} negative>
-									{t(CORE_TRANSLATIONS['BUTTON_CANCEL'])}
-								</Button>
-								<Button
-									iconLeft={isLoading ? 'circle-o-notch fa-spin' : null}
-									disabled={isLoading || isDeleting}
-									className="u-margin-left-xs"
-									onClick={submitForm}
-									type="success"
-								>
-									{t(CORE_TRANSLATIONS['BUTTON_SAVE'])}
-								</Button>
-							</div>
-						</ActionBarContentSection>
-					</ActionBar>
-				</>
-			)}
+						{onDelete ? renderDelete() : null}
+						<ActionBar className="o-action-bar--fixed" isOpen>
+							<ActionBarContentSection>
+								<div className="u-wrapper row end-xs">
+									<Button onClick={onCancel} negative>
+										{t(CORE_TRANSLATIONS['BUTTON_CANCEL'])}
+									</Button>
+									<Button
+										iconLeft={isLoading ? 'circle-o-notch fa-spin' : null}
+										disabled={isLoading || isDeleting || !isChanged}
+										className="u-margin-left-xs"
+										onClick={submitForm}
+										type="success"
+									>
+										{t(CORE_TRANSLATIONS['BUTTON_SAVE'])}
+									</Button>
+								</div>
+							</ActionBarContentSection>
+						</ActionBar>
+					</>
+				);
+			}}
 		</Formik>
 	);
 };
