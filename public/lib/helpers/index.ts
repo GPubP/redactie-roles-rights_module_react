ad1fdb3f81e88cc6ae35430932306ac54fdaddb7
De Weerdt Maarten <maarten.deweerdt@cronos.be>
2021-03-05 12:55:30 +0100

    chore: filenames

--- public/lib/helpers/index.ts
+++ public/lib/helpers/index.ts
@@ -1,5 +1,5 @@
-export { default as mapUserRoles } from './MapUserRoles/MapUserRoles';
-export { default as sortUserRoles } from './SortUserRoles/SortUserRoles';
+export { default as mapUserRoles } from './mapUserRoles/mapUserRoles';
+export { default as sortUserRoles } from './sortUserRoles/SortUserRoles';
 export { default as checkSecurityRights } from './checkSecurityRights/checkSecurityRights';
 export { default as alertService } from './alertService/alertService';
 export * from './checkSecurityRights/checkSecurityRights.types';
